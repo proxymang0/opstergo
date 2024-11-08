5d7b5301f709d42aca100dcec1d8b20931046d7f
luka <lukaslade@proton.me>
2024-11-08 12:57:41 +0100

    Update 03-redis-import.sh

--- 03-redis-import.sh
+++ 03-redis-import.sh
@@ -2,6 +2,16 @@
 
 set -e
 
+# Load variables
+source ./_init_variables.sh
+
+# Get the name of the redis-master pod
+REDIS_MASTER_POD=$(kubectl -n redis get pods -l app=redis-master -o jsonpath='{.items[0].metadata.name}')
+
+# Get the container name inside the redis-master pod
+CONTAINER_NAME=$(kubectl -n redis get pod "$REDIS_MASTER_POD" -o jsonpath='{.spec.containers[0].name}')
+
 # Import data into Redis master
-kubectl -n redis exec -i $(kubectl -n redis get pods -l app=redis-master -o jsonpath='{.items[0].metadata.name}') -- redis-cli < import/redis/data.txt
+kubectl -n redis exec -i "$REDIS_MASTER_POD" -c "$CONTAINER_NAME" -- redis-cli < import/redis/data.txt
+
 echo "Data imported into Redis master."
